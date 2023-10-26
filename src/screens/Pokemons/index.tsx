c92c3daf7a036cbd506c5f6180c7da6abf3aadae
ghccaixeta <gustavohcaixeta@gmail.com>
2023-10-26 08:37:45 -0300

    Instala react-navigation

--- src/screens/Pokemons/index.tsx
+++ src/screens/Pokemons/index.tsx
@@ -1,8 +1,21 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Description } from "@components/Description";
+import { api } from "@services/api";
+
 
 export function Pokemons() {
+
+    async function fetchPokemons() {
+        const response = await api.get('/pokemons?limit=20');
+
+        console.log(response);
+    }
+
+    useFocusEffect(useCallback(() =>{
+        fetchPokemons();
+    }, []))
+
     return(
         <Container>
             <Header title="Pokedev" />
