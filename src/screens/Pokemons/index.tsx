17350d6c2131d9903ef6a5cf1f2930338236224b
ghccaixeta <gustavohcaixeta@gmail.com>
2023-10-26 09:04:21 -0300

    Fetch Pokemons

--- src/screens/Pokemons/index.tsx
+++ src/screens/Pokemons/index.tsx
@@ -2,14 +2,23 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Description } from "@components/Description";
 import { api } from "@services/api";
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback, useState } from "react";
+import { ActivityIndicator, Alert } from "react-native";
 
 
 export function Pokemons() {
 
-    async function fetchPokemons() {
-        const response = await api.get('/pokemons?limit=20');
+    const [isLoading, setIsLoading] = useState(true);
 
-        console.log(response);
+    async function fetchPokemons() {
+        try {
+            const response = await api.get('/pokemon',{params: {limit: 20}});
+        } catch (error) {
+            return Alert.alert("Oops!","Ocorreu um erro. Tente novamente mais tarde.")
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     useFocusEffect(useCallback(() =>{
